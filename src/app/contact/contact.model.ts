335fd53902d632054b99f0455c3689de52ff6719
Jose Hernandez <puch007@mail.fresnostate.edu>
2019-12-03 10:21:49 -0800

    fix delete button

--- src/app/contact/contact.model.ts
+++ src/app/contact/contact.model.ts
@@ -21,14 +21,14 @@ export class Contact {
         Object.assign(this, contact);
     }
     setState(contact: IContact) {
-        if(contact == null || Object.keys (contact) .length == 0){
+        if(contact == null || Object.keys (contact) .length === 0) {
             return true;
         }
 
         let editing = false;
         Object.keys(contact).forEach((key) => {
-            console.log('from setState...', contact[key])
-            if(contact[key] == null){
+            console.log('from setState...', contact[key]);
+            if (contact[key] == null) {
                 editing = true;
             }
         });
